d1143e4fb6ce442ee3ecc3abfaf72fbf52252105
caozilong <caozilong@zuoyebang.com>
2024-06-21 13:48:31 +0800

    variables

---
+++ Chapter01/variables.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+    let mut target = "world";
+    let mut greeting = "hello";
+    println!("{}, {}!", greeting, target);
+    greeting = "How are you doing";
+    println!("{}, {}!", greeting, target);
+    target = "Rust";
+    println!("{}, {}!", greeting, target);
+}
